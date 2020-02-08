ea38e2f9fba463988be5d383906475039b50c63e
anikarabenhorst <anikacchase@gmail.com>
2020-02-08 17:21:11 -0500

    made new js functions

--- myscripts.js
+++ myscripts.js
@@ -27,4 +27,45 @@ function saveform() {
         names.push(item.innerHTML.trim());
     })
     console.log(names);
+
+    var newNames = arr.map(function(val, index){ 
+        return {key:index, value: val}; 
+    }) 
+
+    var results = runAlgo(newNames);
 }
+
+function runAlgo(newNames) {
+    var noPP;
+    var pop;
+    var pop13to44f;
+    var support13to44;
+    var belowPov;
+    var supportBelow20;
+
+    for (i = 0; i < newNames.length(); i++) {
+        switch (newNames[i][1]) {
+            case "New County":
+                noPP = newNames[i][0];
+                break;
+            case "Population" :
+                pop = newNames[i][0];
+                break;
+            case "Population of Women Aged 13-44" :
+                pop13to44f = newNames[i][0];
+                break;
+            case "Number of women who likely need public support for contraceptive services and supplies, aged 13-44" :
+                support13to44 = newNames[i][0];
+                break;
+            case "Number of women who likely need public support for contraceptive services and supplies, aged 20-44 and below the federal poverty level" :
+                belowPov = newNames[i][0];
+                break;
+            case "Number of women who likely need public support for contraceptive services and supplies, younger than 20" :
+                pop = newNames[i][0];
+                break;                
+        }
+    }
+
+    
+}
+
